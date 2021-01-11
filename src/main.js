9b81aaf74809ffbc173599ad0c0d9d0533bec793
Yaroslav Semenov <semyarvlad@gmail.com>
2021-01-11 17:00:19 +0300

    add method getPaths

--- src/main.js
+++ src/main.js
@@ -1,12 +1,37 @@
-const fs = require('fs');
-const { resolve } = require('path');
-const _ = require('lodash');
+const fs = require('file-system');
+const config = require('../settings/config');
 
-const newName = 'my component';
+const getPaths = (dir, result) => {
+  result = result || [];
 
-const toCamelCase = (str) => {
-  return _.upperFirst(_.camelCase(str));
+  const files = fs.readdirSync(dir);
+
+  for (const i in files) {
+    const path = `${dir}/${files[i]}`;
+
+    if (fs.statSync(path).isDirectory()) {
+      getPaths(path, result);
+    } else if (!/\_script_.js$/.test(path)) {
+      result.push(path);
+    }
+  }
+
+  return result;
 };
 
-console.log('camel: ', toCamelCase(newName));
-console.log('kebab: ', _.kebabCase(newName));
+const templatesPath = 'C:/Users/mail/Desktop/timoha/arhitect/architect/settings/templates';
+
+const allPath = getPaths(templatesPath);
+console.log(allPath);
+// const regExp = `/(?<=${config.itrStart}).+(?=${config.itrEnd})/gm`;
+//
+// const filePath = 'C:/Users/mail/Desktop/timoha/arhitect/architect/settings/templates/index/index.ts.txt';
+//
+// const file = async (filePath) => {
+//   fs.readFile(filePath, 'utf8', (err, contents) => {
+//     if (err) throw err;
+//
+//     console.log(contents);
+//   });
+// };
+// file(filePath);
